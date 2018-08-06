f3684ca7c0f830cafbeed5b2c1e13f50bb609197
Jarris3154 <hu.jia.jarris@gmail.com>
2018-08-06 21:38:08 +0800

    refactor: add getAssetList

--- src/main/java/com/jia/Dvd/controller/AssetController.java
+++ src/main/java/com/jia/Dvd/controller/AssetController.java
@@ -3,10 +3,9 @@ package com.jia.Dvd.controller;
 import com.jia.Dvd.model.Asset;
 import com.jia.Dvd.service.AssetService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/rest/v1/assets")
@@ -18,5 +17,10 @@ public class AssetController {
     @PostMapping
     public Asset add(@RequestBody Asset asset){
         return assetService.save(asset);
+    }
+
+    @GetMapping
+    public List<Asset> get(){
+        return assetService.findAll();
     }
 }
--- src/main/java/com/jia/Dvd/service/AssetService.java
+++ src/main/java/com/jia/Dvd/service/AssetService.java
@@ -5,6 +5,8 @@ import com.jia.Dvd.repository.AssetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AssetService {
 
@@ -13,5 +15,9 @@ public class AssetService {
 
     public Asset save(Asset asset) {
         return assetRepository.save(asset);
+    }
+
+    public List<Asset> findAll() {
+        return assetRepository.findAll();
     }
 }
